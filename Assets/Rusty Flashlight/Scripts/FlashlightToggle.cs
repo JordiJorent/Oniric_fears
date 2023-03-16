309992dd6156858d29f076ed9455309cfe888c14
widepeepohappysanu <ernest.lloberas@gmail.com>
2023-03-16 02:34:23 +0100

    linterna

---
+++ Assets/Rusty Flashlight/Scripts/FlashlightToggle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class FlashlightToggle : MonoBehaviour
+{
+    public GameObject lightGO; 
+    private bool isOn = false; 
+
+    void Start()
+    {
+        lightGO.SetActive(isOn);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isOn = !isOn;
+            if (isOn)
+            {
+                lightGO.SetActive(true);
+            }
+            else
+            {
+                lightGO.SetActive(false);
+
+            }
+        }
+    }
+}
--- Assets/control.cs
+++ Assets/control.cs
@@ -8,10 +8,9 @@ public class FirstPersonMovement : MonoBehaviour
     [Header("Running")]
     public bool canRun = true;
     public bool IsRunning { get; private set; }
-    public float runSpeed = 9;
+    public float runSpeed = 8;
     public KeyCode runningKey = KeyCode.LeftShift;
-
-    Rigidbody rigidbody;
+    public  Rigidbody rigidbody;
     /// <summary> Functions to override movement speed. Will use the last added override. </summary>
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
 
