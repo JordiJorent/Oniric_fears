4248dde43815f36098d7d790d118360e9ce4c679
waidenart <aidalazaro2002@gmail.com>
2023-03-30 17:16:15 +0200

    se ha intentado(screampasos)

--- Assets/Code/jumpscares/pasos.cs
+++ Assets/Code/jumpscares/pasos.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class pasos : MonoBehaviour
 {
+    public float time;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class pasos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        transform.position += transform.forward * speed *Time.deltaTime;
     }
 }
