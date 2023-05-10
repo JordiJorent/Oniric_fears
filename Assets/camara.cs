cccc7b90a69dd9f968d3b61db8c8f1849e69dbe2
JordiJVilarasau <jordijornet866@gmail.com>
2023-05-10 17:55:31 +0200

    camaras programadas

---
+++ Assets/camara.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;  
+public class camara : MonoBehaviour
+{
+    
+    private Transform Player;
+    private Rigidbody rb;
+    private Quaternion lastQuat;
+    [SerializeField] private float minDistanceBtwnPlayerAndCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player != null)
+        {
+            if(Vector3.Distance(transform.position, Player.position) < minDistanceBtwnPlayerAndCamera)
+            {
+                transform.rotation = lastQuat;
+            }
+            else
+            {
+                transform.LookAt(Player.position);
+                lastQuat = transform.rotation;
+            }
+            
+            
+        }
+    }
+}
