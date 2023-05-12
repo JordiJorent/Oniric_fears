aaed202e0f69e41977ce482da276bf2f8539b079
waidenart <aidalazaro2002@gmail.com>
2023-05-12 12:14:21 +0200

    funciona darle al pause

---
+++ Assets/Menu/PauseMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool pause;
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause = !pause;
+            pauseMenu.SetActive(pause);
+        }
+    }
+}
