d3df41eb1e6ee9418bb6761a2074ed219041f5ac
ernestXI <ernest.lloberas@gmail.com>
2023-05-29 17:29:26 +0200

    skip

--- Assets/Level/Scenes/animacion/animacion.cs
+++ Assets/Level/Scenes/animacion/animacion.cs
@@ -5,15 +5,53 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class animacion : MonoBehaviour
+
 {
+    Vector3 AnteriorPosition;
     public VideoPlayer videoPlayer;
     public string nombreDeLaEscena;
+    public GameObject Boton;
+    float TemporizadorActual;
+    public float TiempoMaximo;
+
+
 
     private void Start()
     {
+        TemporizadorActual = 0.0f;
         videoPlayer.loopPointReached += CambiarEscena;
+        AnteriorPosition = Input.mousePosition;
+        Boton.SetActive(false);
+
     }
-    private void CambiarEscena(VideoPlayer vp)
+
+    private void Update()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            if (Boton.activeSelf)
+            {
+                TemporizadorActual += Time.deltaTime;
+                if (TemporizadorActual >= TiempoMaximo)
+                {
+                    Boton.SetActive(false);
+                    TemporizadorActual = 0.0f;
+                }
+            }
+
+
+            if (Input.mousePosition != AnteriorPosition)
+            {
+                AnteriorPosition = Input.mousePosition;
+                Boton.SetActive(true);
+                TemporizadorActual = 0.0f;
+            }
+
+
+        }
+
+    }
+    public void CambiarEscena(VideoPlayer vp)
     {
         SceneManager.LoadScene(nombreDeLaEscena);
 
@@ -22,5 +60,5 @@ public class animacion : MonoBehaviour
     }
 
 
-
 }
+
